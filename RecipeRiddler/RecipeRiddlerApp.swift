5c1bde24e90e360872097f01927d0345d18fd0e5
Krishna Pothuganti <kspothuganti@gmail.com>
2024-11-25 04:54:18 -0600

    Initial Commit

---
+++ RecipeRiddler/RecipeRiddlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeRiddlerApp.swift
+//  RecipeRiddler
+//
+//  Created by Krishna Pothuganti on 11/25/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeRiddlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
